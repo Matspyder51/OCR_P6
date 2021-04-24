cf8ac9eabfa1e44d452443aa9d893c88011301ac
Mathis Cucci <mat.spyder@gmail.com>
2021-04-24 19:52:56 +0200

    Create function to quickly get movies

--- index.js
+++ index.js
@@ -51,4 +51,42 @@ function SlideLeft(elem) {
 	// 	elem.classList.add('disabled');
 }
 
-OpenModal()
+// OpenModal()
+
+async function GetMoviesOfCategory(name, amount = 7, sorted_by = "&sort_by=-votes,-imdb_score") {
+	const results = await fetch(`http://localhost:8000/api/v1/titles/?genre=${name}${sorted_by}`);
+
+	if (!results.ok)
+		return
+
+	const data = await results.json();
+	let result = Array(...data.results);
+
+	if (result.length < amount) {
+		const results2 = await (await fetch(data.next)).json();
+
+		result.push(...Array(...results2.results).slice(0, amount - result.length));
+	}
+
+	return result;
+}
+
+async function GetMovieInfos(id) {
+	const results = await fetch(`http://localhost:8000/api/v1/titles/${id}`);
+
+	if (!results.ok)
+		return;
+
+	const result = await results.json();
+
+	return result;
+}
+
+document.addEventListener('load', (ev) => {
+
+});
+
+// GetMovieInfos();
+(async () => {
+	console.log((await GetMoviesOfCategory('action')));
+})();
